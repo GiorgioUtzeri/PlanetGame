479da2dc1b2f58b6f3a458c03b28f976c362a592
Giorgio <84821764+GiorgioUtzeri@users.noreply.github.com>
2023-12-18 10:10:21 +0100

    Delete B_UTZERI_Giorgio_B_LANNOY_Noa_PlanetGame directory

--- B_UTZERI_Giorgio_B_LANNOY_Noa_PlanetGame/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export SOURCES=src
-export CLASSES=classes
-export CLASSPATH=`find lib -name "*.jar" | tr '\n' ':'`
-
-javac -cp ${CLASSPATH} -sourcepath ${SOURCES} -d ${CLASSES} $@ `find src -name "*.java"`
--- B_UTZERI_Giorgio_B_LANNOY_Noa_PlanetGame/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export CLASSPATH=`find ./lib -name "*.jar" | tr '\n' ':'`
-export MAINCLASS=PlanetGame # <- à remplacer par le nom de votre programme
-java -cp ${CLASSPATH}:classes $MAINCLASS
-# Le programme s'exécute depuis la racine de l'archive
